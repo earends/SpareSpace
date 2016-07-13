cb10019bd3eb88b2324f6251f121f5845e66ab40
Evan Arends <evanarendss@gmail.com>
2016-07-13 08:09:20 -0700

    Added picture taking capabilities

--- SpareSpace/app/src/main/java/com/evanarendssgmail/sparespace/sell.java
+++ SpareSpace/app/src/main/java/com/evanarendssgmail/sparespace/sell.java
@@ -1,8 +1,14 @@
 package com.evanarendssgmail.sparespace;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class sell extends Activity {
 
@@ -10,5 +16,24 @@ public class sell extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void upload_click(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image.setImageBitmap(imageBitmap);
+        }
     }
 }
