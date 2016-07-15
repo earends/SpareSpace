d7dc33832b99c2c780c104a7622cd734d2cb2ca4
Evan Arends <evanarendss@gmail.com>
2016-07-14 20:07:31 -0700

    added new layout for the login screen

--- SpareSpace/app/src/main/java/com/evanarendssgmail/sparespace/Login.java
+++ SpareSpace/app/src/main/java/com/evanarendssgmail/sparespace/Login.java
@@ -1,15 +1,10 @@
 package com.evanarendssgmail.sparespace;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,19 +15,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        /*
         final EditText etUsername = (EditText) findViewById(R.id.login_username);
         final EditText etPassword = (EditText) findViewById(R.id.login_password);
         final TextView bLogin = (TextView) findViewById(R.id.login_button);
@@ -57,13 +47,13 @@ public class MainActivity extends AppCompatActivity {
                                 String name = jsonResponse.getString("name");
                                 int age = jsonResponse.getInt("age");
 
-                                Intent intent = new Intent(MainActivity.this, UserArea.class);
+                                Intent intent = new Intent(Login.this, Home_page.class);
                                 intent.putExtra("name", name);
                                 intent.putExtra("age", age);
                                 intent.putExtra("username", username);
-                                MainActivity.this.startActivity(intent);
+                                Login.this.startActivity(intent);
                             } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
                                 builder.setMessage("Login Failed")
                                         .setNegativeButton("Retry", null)
                                         .create()
@@ -77,49 +67,14 @@ public class MainActivity extends AppCompatActivity {
                 };
 
                 LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                RequestQueue queue = Volley.newRequestQueue(Login.this);
                 queue.add(loginRequest);
             }
         });
-        */
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * What happens when the user presses the login Button
-     * @param v
-     */
-    /*
-    public void login_click(View v) {
-        Intent i = new Intent(getApplicationContext(), Home_page.class);
-        startActivity(i);
-    }
-    */
-    /*
     public void register_click(View v) {
         Intent i = new Intent(getApplicationContext(),Register.class);
         startActivity(i);
     }
-    */
 }
