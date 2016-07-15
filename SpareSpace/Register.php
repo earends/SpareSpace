ce9376f035b715e7f3cfffa8174f07a112c3c0d5
Evan Arends <evanarendss@gmail.com>
2016-07-14 18:49:59 -0700

    just fixed login and register fuck yes!!

--- SpareSpace/Register.php
+++ SpareSpace/Register.php
@@ -2,11 +2,11 @@
     $con = mysqli_connect("mysql13.000webhost.com", "a8457156_Sspace", "discord_space123", "a8457156_Sspace");
     
     $name = $_POST["name"];
-    $age = $_POST["age"];
     $username = $_POST["username"];
+    $age = $_POST["age"];
     $password = $_POST["password"];
-    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, age, username, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name,$age, $username, $password);
     mysqli_stmt_execute($statement);
     
     $response = array();
